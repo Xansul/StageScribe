de1277e91eca622cc73aa82c3f801ce1b4f766c2
Tyler Call <ty.call13@gmail.com>
2022-04-07 17:47:47 -0600

    main and scribe class rework

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
-import tkinter
-from tkinter.font import NORMAL
-from tracemalloc import start
 import scribe
 
-def begin(scribe, start_button, end_button):
-    #call scribe function
-    scribe.begin_record()
-
-    #update button states
-    start_button["state"] = tkinter.DISABLED
-    end_button["state"] = tkinter.NORMAL
-
-def end(scribe, start_button, end_button):
-    #call scribe function
-    scribe.end_recording()
-
-    #update button states
-    start_button["state"] = tkinter.NORMAL
-    end_button["state"] = tkinter.DISABLED
-
-#create scribe object
-scribeObject = scribe.scribe()
-
-#setup tkinter window
-window = tkinter.Tk()
-window.title("StageScribe")
-
-#setup buttons
-start_record_button = tkinter.Button(window, text="Begin Recording", command=lambda: begin(scribeObject, start_record_button, end_record_button))
-end_record_button = tkinter.Button(window, text="End Recording", command=lambda: end(scribeObject, start_record_button, end_record_button))
-
-start_record_button.pack()
-end_record_button.pack()
-
-end_record_button["state"] = tkinter.DISABLED
-
-#setup text box
-text = tkinter.Text(window, height=8)
-text.pack()
-
 #start window
-window.mainloop()
+scribeObject = scribe.scribe()
+scribeObject.setup_window()
+scribeObject.window.mainloop()
--- scribe.py
+++ scribe.py
@@ -7,6 +7,11 @@
 
 import deepspeech
 import recorder
+import wave
+import numpy
+import tkinter
+from tkinter.font import NORMAL
+from tracemalloc import start
 
 class scribe():
 
@@ -25,12 +30,71 @@ class scribe():
     beam_width = 500
     model.setBeamWidth(beam_width)
 
-    #recorder setup
-    recorder = recorder.RecordingFile("recording", "wb", 2, 16000, 1024)
-
     def begin_record(self):
         self.recorder.start_recording()
 
     def end_recording(self):
         self.recorder.stop_recording()
         self.recorder.close()
+
+    def process_recording(self):
+        #wave file setup
+        filename = self.recorder.fname
+        w = wave.open(filename, "r")
+        rate = w.getframerate()
+        frames = w.getnframes()
+        buffer = w.readframes(frames)
+
+        #convert buffer to 16 bit array
+        data16 = numpy.frombuffer(buffer, dtype=numpy.int16)
+
+        #process text
+        self.result_text = self.model.stt(data16)
+
+        
+    def begin_helper(self, start_button, end_button):
+        #recorder setup
+        #set channel count to 1 for bluetooth mic, 2 for laptop mic
+        self.recorder = recorder.RecordingFile("ScribeRecording.wav", "wb", 1, 16000, 1024)
+
+        #call scribe function
+        self.begin_record()
+
+        #update button states
+        start_button["state"] = tkinter.DISABLED
+        end_button["state"] = tkinter.NORMAL
+        
+
+    def end_helper(self, start_button, end_button, text_box):
+        #call scribe function
+        self.end_recording()
+
+        #update button states
+        start_button["state"] = tkinter.NORMAL
+        end_button["state"] = tkinter.DISABLED
+
+        #process audio
+        self.process_recording()
+
+        #add text from audio stt to text box
+        result = self.result_text
+        text_box.insert("1.0", result)
+
+
+    def setup_window(self):
+        #setup tkinter window
+        self.window = tkinter.Tk()
+        self.window.title("StageScribe")
+
+        #setup text box
+        text = tkinter.Text(self.window, height=8)
+        text.pack()
+
+        #setup buttons
+        start_record_button = tkinter.Button(self.window, text="Begin Recording", command=lambda: self.begin_helper(start_record_button, end_record_button))
+        end_record_button = tkinter.Button(self.window, text="End Recording", command=lambda: self.end_helper(start_record_button, end_record_button, text))
+
+        start_record_button.pack()
+        end_record_button.pack()
+
+        end_record_button["state"] = tkinter.DISABLED
